aa24640388cf55a7ed0702e9d4a1d53684f2668e
rohit <rkg10072000@gmail.com>
2022-06-30 19:32:32 +0530

    configured webpack for images and svgs

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,15 @@ module.exports = {
       {
         test: /.(css|scss|sass)$/,
         use: ["style-loader", "css-loader", "sass-loader"]
-      }
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ]
   },
   plugins: [new HtmlWebpackPlugin({
